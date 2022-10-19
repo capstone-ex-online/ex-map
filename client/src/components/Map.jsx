ee21a97f207b196d25177db96bd17c07c53ea643
조성현 <tjdgus772266@gmail.com>
2022-10-19 21:08:42 +0900

    지도기능추가

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -3,19 +3,21 @@ import styled from 'styled-components'
 
 const { kakao } = window;
 
+
+
 const MapContainer = styled.div`
-  width: 100%;
-  height: 100%;
+  width: 90%;
+  height: 90%;
   display: inline-block;
 `;
 
 const MapBox = styled.div`
-  width: 1099px;
-  height: 865px;
+  width: 1300px;
+  height: 765px;
 
   #map {
-    width: 1099px;
-    height: 865px;
+    width: 1300px;
+    height: 765px;
   }
 `
 
@@ -25,10 +27,21 @@ function Map() {
   //처음 지도 그리기
   useEffect(()=>{
       const container = document.getElementById('map');
-      const options = { center: new kakao.maps.LatLng(33.450701, 126.570667), level: 3 };
+      const options = { center: new kakao.maps.LatLng(37.4945067, 126.959837), level: 3 };
       const kakaoMap = new kakao.maps.Map(container, options);
+      const mapTypeControl = new kakao.maps.MapTypeControl(); // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+      kakaoMap.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+      const zoomControl = new kakao.maps.ZoomControl(); // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+      kakaoMap.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
       setMap(kakaoMap);
   },[])
+
+  function setDraggable(draggable) {
+    // 마우스 드래그로 지도 이동 가능여부를 설정합니다
+    kakaoMap.setDraggable(draggable);    
+}
+
+
 
   return (
     <MapContainer>
