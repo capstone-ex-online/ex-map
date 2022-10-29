a4f1b8155f915c84c109a98ba0bf0c3e4c756b6a
leehyewon0531 <ifpffk@naver.com>
2022-10-29 22:32:56 +0900

    feat: 마커 클릭시 해당 위치 전시 목록 출력

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -4,6 +4,8 @@ import Axios from 'axios'
 
 const { kakao } = window;
 
+let mappingList = {};
+
 const MapContainer = styled.div`
   width: 100%;
   height: 100%;
@@ -20,8 +22,14 @@ const MapBox = styled.div`
   }
 `
 
+const ExhibitionList = styled.div`
+  width: 500px;
+  height: 500px;
+`
+
 function Map() {
   const [map, setMap] = useState(null);
+  const [exhiList, setExhiList] = useState([])
 
   let imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"
 
@@ -35,7 +43,6 @@ function Map() {
 
   Axios.get('http://localhost:5000/api/exhibition')
   .then((response) => {
-    // console.log(response.data)
     makeMarkers(response.data)
   })
 
@@ -51,8 +58,45 @@ function Map() {
       let marker = new kakao.maps.Marker({
         map: map,
         position: position,
-        image: markerImage
+        image: markerImage,
       })
+
+      let object = {
+        marker: marker,
+        data: {
+          seq: array[i].seq._text,
+          title: array[i].title._text,
+          startDate: array[i].startDate._text,
+          endDate: array[i].endDate._text,
+          thumbnail: array[i].thumbnail._text
+        }
+      }
+
+      let temp_id = 'id' + i
+      mappingList[temp_id] = object
+
+      kakao.maps.event.addListener(marker, 'click', function() {    
+        setExhiList([])    
+        let values = Object.values(mappingList)
+
+        for(let j = 0; j < values.length; j++) {
+          if(values[j].marker === marker) {
+            // if(exhiList.length === 0) {
+            //   setExhiList(values[j].data)
+            //   console.log(exhiList)
+            // } else {
+            //   let temp_arr = new Array(exhiList)
+            //   temp_arr.push(values[j].data)
+            //   setExhiList(temp_arr)
+            //   console.log(exhiList)
+            // }
+            setExhiList(new Array(values[j].data))
+            console.log(exhiList)
+          }
+        }
+
+      })
+
     }
   }
   
@@ -61,6 +105,23 @@ function Map() {
       <MapBox>
         <div id='map'></div>
       </MapBox>
+      <ExhibitionList
+        exhiList={exhiList}
+      >
+        {exhiList && exhiList.map((exhibition, index) => {
+          return (
+            <div key={index}>
+              <h2>title: {exhibition.title}</h2>
+              <h2>startDate: {exhibition.startDate}</h2>
+              <h2>endDate: {exhibition.endDate}</h2>
+              <div style={{width: 100 + 'px', 
+                height: 100 + 'px', 
+                backgroundImage: `url(${exhibition.thumbnail})`,
+                backgroundSize: 100 + 'px'}}  ></div>
+            </div>
+          )
+        })}
+      </ExhibitionList>
     </MapContainer>
   )
 }
